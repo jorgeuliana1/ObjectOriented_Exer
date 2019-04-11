49ead1e87a151f1661ca4ced4dc4ba9bd499f6bc
jorgeuliana1 <ulianamjjorge@gmail.com>
2019-04-11 12:04:34 -0300

    J2_04 started.

---
+++ boca/J2_04/CompactDisc.java
@@ -0,0 +1,18 @@
+public class CompactDisc extends Item {
+    private int tracks;
+
+    public CompactDisc(String name, double price, int tracks) {
+        super(name, price);
+        this.tracks = tracks;
+    }
+
+    public int getTracks() {
+        return tracks;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " (R$"this.price + ") - " + this.tracks;
+    }
+    
+}
---
+++ boca/J2_04/J2_04.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.Formatter;
+
+public class J2_04 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    }
+}
