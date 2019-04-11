3922c8106580d018d762b99a508d3dc020eaa0e4
jorgeuliana1 <ulianamjjorge@gmail.com>
2019-04-11 12:15:58 -0300

    Main started.

--- boca/J2_04/J2_04.java
+++ boca/J2_04/J2_04.java
@@ -3,7 +3,18 @@ import java.util.Formatter;
 
 public class J2_04 {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
 
+        int n;
+        n = in.nextInt();
+        for(int i = 0; i < n; i++) {
+            String s = in.nextLine();
+            String[] s2 = s.split(",");
+
+            if(s[0].equals("L")) {
+                
+            }
+        }
     }
 }
