954edf8865e4b74310ea6b3ad9c13fecc3851796
jorgeuliana1 <ulianamjjorge@gmail.com>
2019-04-12 09:45:26 -0300

    J2_05 finished.

--- boca/J2_05/J2_05.java
+++ boca/J2_05/J2_05.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class J2_05 {
     public static void main(String[] args) {
@@ -11,13 +12,7 @@ public class J2_05 {
         int n;
         n = Integer.parseInt(s1);
 
-        Book[] b;
-        CompactDisc[] c;
-        MovieDisc[] d;
-
-        b = new Book[n];
-        c = new CompactDisc[n];
-        d = new MovieDisc[n];
+        ArrayList<Item> a = new ArrayList<Item>();
         
         for(int i = 0; i < n; i++) {
             
@@ -25,75 +20,29 @@ public class J2_05 {
             String[] s2 = s.split(",");
 
             
-            if(s2[0].equals("L")) {
-                for(int j = 0; j < (b.length - 1) && b[j] != null; j++) {
-                    if(b[j + 1] == null) {
-                        b[j + 1] = new Book(Integer.parseInt(s2[1]), s2[2], Double.parseDouble(s2[3]), s2[4]);
-                    }
-                }
-            }
-
-            else if(s2[0].equals("C")) {
-                for(int j = 0; j < (c.length - 1) && c[j] != null; j++) {
-                    if(c[j + 1] == null) {
-                        c[j + 1] = new CompactDisc(Integer.parseInt(s2[1]), s2[2], Double.parseDouble(s2[3]), Integer.parseInt(s2[4]));
-                    }
-                }
-            }
-
-            else if(s2[0].equals("D")) {
-                for(int j = 0; j < (d.length - 1) && d[j] != null; j++) {
-                    if(d[j + 1] == null) {
-                        d[j + 1] = new MovieDisc(Integer.parseInt(s2[1]), s2[2], Double.parseDouble(s2[3]), Integer.parseInt(s2[4]));
-                    }
-                }
-            }
+            a.add(new Item(Integer.parseInt(s2[1]), s2[2], Double.parseDouble(s2[3])));
             
         }
 
         String s = in.nextLine();
         String[] s2 = s.split(",");
 
-        if(s2[0].equals("L")) {
-            int r = searchIndex(b, Integer.parseInt(s2[1]));
-            if(r == -1)
-                System.out.println("Produto nao encontrado.");
-            else
-                System.out.println(Integer.toString(r));
-        }
+        int r = searchIndex(a, Integer.parseInt(s2[1]));
+        if(r == -1)
+            System.out.println("Produto nao encontrado.");
+        else
+            System.out.println(Integer.toString(r));
 
-        else if(s2[0].equals("C")) {
-            int r = searchIndex(c, Integer.parseInt(s2[1]));
-            if(r == -1)
-                System.out.println("Produto nao encontrado.");
-            else
-                System.out.println(Integer.toString(r));
-        }
-
-        else if(s2[0].equals("D")) {
-            int r = searchIndex(d, Integer.parseInt(s2[1]));
-            if(r == -1)
-                System.out.println("Produto nao encontrado.");
-            else
-                System.out.println(Integer.toString(r));
-        }
 
     }
 
-    static int searchIndex(Item[] i, int i2) {
+    static int searchIndex(ArrayList<Item> i, int i2) {
 
-        System.out.println(i2 + " ");
+        for(int j = 0; j < i.size(); j++) {
 
-        for(int j = 0; j < i.length; j++) {
-
-            System.out.println(j + " ");
-
-            if(i[j] == null) {
-                System.out.println("DEURUIM");
-                break;
-            }
-            if(i[j].equals(i2))
+            if(i.get(j).equals(i2))
                 return j;
+                
         }
 
         return -1;
