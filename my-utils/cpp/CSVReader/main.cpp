acfdf845496e84d6c65e7236bea77d40ca6a11e0
jorgeuliana1 <ulianamjjorge@gmail.com>
2019-06-19 20:42:30 -0300

    One more test file added.

--- my-utils/cpp/CSVReader/main.cpp
+++ my-utils/cpp/CSVReader/main.cpp
@@ -9,7 +9,7 @@ using namespace csv_reader;
 
 int main() {
     // TESTING THE LIB
-    CSVReader csv("input1.csv", ";", true);
+    CSVReader csv("input1.csv", ",", true);
     while(!csv.eof()) {
         csv.next();
         std::cout << csv.getFromCachedLine("Tile") << " " << csv.getFromCachedLine("Genre") << std::endl;
